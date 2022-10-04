6e9fa30daf82f6afa9ae67e167df6d5384ac0b3b
0xBingo ( ͡° ͜ʖ ͡°) <100392110+Oxbingo@users.noreply.github.com>
2022-10-04 20:43:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
          $message = "<b>This is HTML message.</b>";
          $message .= "<h1>This is headline.</h1>";
          
-         $header = "From:mugenfinance.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
+         $header = "From:bingo@mugenfinance.com \r\n";
+         $header .= "Cc:bingo@mugenfinance.com \r\n";
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          
